4c3e6502dae81c20a7a9a2380549c70f0da2c900
Thiago <thiago.ralves02@gmail.com>
2022-01-22 15:29:02 -0300

    feat: componente Home iniciado

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import Funcionarios from "../../components/Funcionarios/Funcionarios";
+import Navbar from "../../components/Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Funcionarios />
+      </main>
+    </>
+  );
+}
+
+export default App;

099939d79f6b2f66e8e827a7a5f4e020241a58f6
Thiago <thiago.ralves02@gmail.com>
2022-01-22 16:10:21 -0300

    fix: telefone formatado corretamente

--- src/components/Funcionario/Funcionario.js
+++ src/components/Funcionario/Funcionario.js
@@ -11,20 +11,26 @@ export default function Funcionario({
 }) {
   const novaData = data.split("T00:00:00.000Z").join("");
 
-  function formatarData(input) {
+  const formatarData = (input) => {
     var dataParte = input.match(/\d+/g),
       ano = dataParte[0].substring(0, 4), // get only two digits
       mes = dataParte[1],
       dia = dataParte[2];
 
     return dia + "/" + mes + "/" + ano;
-  }
+  };
 
   const dataFormatada = formatarData(novaData);
 
-  console.log(
-    telefone.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
-  );
+  const formatarTelefone = (numero) => {
+    numero = numero.replace(/\D/g, ""); //Remove tudo o que não é dígito
+    numero = numero.replace(/^(\d{2})(\d{2})/g, "+$1 ($2) "); //Coloca parênteses em volta dos dois primeiros dígitos
+    numero = numero.replace(/(\d)(\d{4})$/, "$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+    return numero;
+  };
+
+  const telefoneFormatado = formatarTelefone(telefone);
+
   return (
     <div className="funcionario-container" key={index}>
       <div>
@@ -33,7 +39,7 @@ export default function Funcionario({
       <p>{nome}</p>
       <p>{cargo}</p>
       <p>{dataFormatada}</p>
-      <p>{telefone}</p>
+      <p>{telefoneFormatado}</p>
     </div>
   );
 }

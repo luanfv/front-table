95beed72f6adb87bc05cef097754f2c511ea009f
Thiago <thiago.ralves02@gmail.com>
2022-01-23 14:51:34 -0300

    feat:  paginas e limite adicionados ao context

--- src/context/CtxApp.js
+++ src/context/CtxApp.js
@@ -5,8 +5,13 @@ const Context = createContext();
 
 export default function CtxApp({ children }) {
   const [funcionarios, setFuncionarios] = useState([]);
+  const [pagina, setPagina] = useState(1);
+  const [limite] = useState(8);
+
   return (
-    <Context.Provider value={{ funcionarios, setFuncionarios }}>
+    <Context.Provider
+      value={{ funcionarios, setFuncionarios, pagina, setPagina, limite }}
+    >
       {children}
     </Context.Provider>
   );
